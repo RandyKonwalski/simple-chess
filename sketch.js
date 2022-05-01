14404baf88b9b488419e75a646354925cf6a68d3
Randy Konwalski <randy.konwalski@gmail.com>
2022-05-01 13:16:48 +0200

    updated sketch

--- sketch.js
+++ sketch.js
@@ -5,9 +5,260 @@ let pieceImages;
 let pieceLocations = [
   {
     type: "king",
+    color: "black",
+    pos: {
+      x: 4,
+      y: 7
+    }
+  },
+  {
+    type: "queen",
+    color: "black",
+    pos: {
+      x: 3,
+      y: 7
+    }
+  },
+  {
+    type: "bishop",
+    color: "black",
+    pos: {
+      x: 2,
+      y: 7
+    }
+  },
+  {
+    type: "bishop",
+    color: "black",
+    pos: {
+      x: 5,
+      y: 7
+    }
+  },
+  {
+    type: "knight",
+    color: "black",
     pos: {
       x: 1,
-      y: 2
+      y: 7
+    }
+  },
+  {
+    type: "knight",
+    color: "black",
+    pos: {
+      x: 6,
+      y: 7
+    }
+  },
+  {
+    type: "rook",
+    color: "black",
+    pos: {
+      x: 0,
+      y: 7
+    }
+  },
+  {
+    type: "rook",
+    color: "black",
+    pos: {
+      x: 7,
+      y: 7
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 0,
+      y: 6
+    }
+  },  
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 1,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 2,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 3,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 4,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 5,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 6,
+      y: 6
+    }
+  },
+  {
+    type: "pawn",
+    color: "black",
+    pos: {
+      x: 7,
+      y: 6
+    }
+  },
+
+
+  {
+    type: "king",
+    color: "white",
+    pos: {
+      x: 4,
+      y: 0
+    }
+  },
+  {
+    type: "queen",
+    color: "white",
+    pos: {
+      x: 3,
+      y: 0
+    }
+  },
+  {
+    type: "bishop",
+    color: "white",
+    pos: {
+      x: 2,
+      y: 0
+    }
+  },
+  {
+    type: "bishop",
+    color: "white",
+    pos: {
+      x: 5,
+      y: 0
+    }
+  },
+  {
+    type: "knight",
+    color: "white",
+    pos: {
+      x: 1,
+      y: 0
+    }
+  },
+  {
+    type: "knight",
+    color: "white",
+    pos: {
+      x: 6,
+      y: 0
+    }
+  },
+  {
+    type: "rook",
+    color: "white",
+    pos: {
+      x: 0,
+      y: 0
+    }
+  },
+  {
+    type: "rook",
+    color: "white",
+    pos: {
+      x: 7,
+      y: 0
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 0,
+      y: 1
+    }
+  },  
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 1,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 2,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 3,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 4,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 5,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 6,
+      y: 1
+    }
+  },
+  {
+    type: "pawn",
+    color: "white",
+    pos: {
+      x: 7,
+      y: 1
     }
   }
 ];
@@ -15,57 +266,67 @@ let pieceLocations = [
 let tileData = [];
 
 const tileSize = 100;
+const margin = 13;
 
 function drawTile(x, y, tileSize, color) {
   fill(color);
   rect(x, y, tileSize, tileSize);
 }
 
-function drawPiece(x, y, piece) {
+function drawPiece(x, y, piece, color) {
   switch (piece) {
     case "king":
       image(
-        pieceImages.king.black,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
+        color === "black" ? pieceImages.king.black : pieceImages.king.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
       );
       break;
     case "queen":
       image(
-        pieceImages.queen.black,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
+        color === "black" ? pieceImages.queen.black : pieceImages.queen.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
       );
       break;
     case "knight":
       image(
-        pieceImages.knight.black,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
+        color === "black" ? pieceImages.knight.black : pieceImages.knight.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
       );
       break;
     case "bishop":
       image(
-        pieceImages.bishop.black,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
+        color === "black" ? pieceImages.bishop.black : pieceImages.bishop.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
+      );
+      break;
+    case "rook":
+      image(
+        color === "black" ? pieceImages.rook.black : pieceImages.rook.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
       );
       break;
     case "pawn":
       image(
-        pieceImages.pawn.black,
-        x * tileSize,
-        y * tileSize,
-        tileSize,
-        tileSize
+        color === "black" ? pieceImages.pawn.black : pieceImages.pawn.white,
+        x * tileSize + margin,
+        y * tileSize + margin,
+        tileSize - margin * 2,
+        tileSize - margin * 2
       );
       break;
     default:
@@ -88,12 +349,13 @@ function drawBoard() {
     drawPiece(
       pieceLocations[i].pos.x,
       pieceLocations[i].pos.y,
-      pieceLocations[i].type
+      pieceLocations[i].type,
+      pieceLocations[i].color
     );
   }
 }
 
-function createTileObject(x, y, posX, posY, color, piece = "none") {
+function createTileObject(x, y, posX, posY, color) {
   return {
     tile: {
       x: x,
@@ -103,9 +365,35 @@ function createTileObject(x, y, posX, posY, color, piece = "none") {
       posX: posX,
       posY: posY
     },
-    piece: piece,
     color: color
   };
+}
+
+function movePiece(fromX, fromY, toX, toY) {
+  let p = pieceLocations.filter(piece => piece.pos.x === fromX && piece.pos.y === fromY);
+  if(p[0]){
+    p[0].pos.x = toX;
+    p[0].pos.y = toY;
+  }
+  else{
+    console.log("Error");
+  }
+}
+
+const ngb = document.getElementById("newGameButton");
+ngb.addEventListener("click", newGameButton);
+
+function newGameButton() {
+  movePiece(0, 0, 4, 4);
+}
+
+// Implement these
+function displayAllowedMoves(piece, x, y) {
+  // Mark squares with red if a move is possible.
+}
+
+function isAllowedMove(piece, x, y, move) {
+  return true;
 }
 
 /**
--- ui.js
+++
@@ -1,6 +0,0 @@
-const ngb = document.getElementById("newGameButton");
-ngb.addEventListener("click", newGameButton);
-
-function newGameButton() {
-  console.log("Hello");
-}
